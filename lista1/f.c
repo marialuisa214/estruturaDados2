48a8b8d233a0c16d71b51be944f68ad9a43fe494
Maria Luisa <maria.luisa.alves214@gmail.com>
2024-03-29 22:03:53 -0300

    :question: :fire: - nivel Ribas

---
+++ lista1/f.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+int xmic, ymic;
+int xBandido = 0, yBandido = 0;
+int minutos = 0;
+
+void invadiu() {
+    if (abs(ymic - yBandido) <= 1 && abs(xmic - xBandido) <= 1) {
+        minutos += 1;
+    }
+}
+
+void novaCoordenada(int direcao) {
+    if (direcao == 1) {
+        yBandido += 1;
+    } else if (direcao == 2) {
+        yBandido -= 1;
+    } else if (direcao == 3) {
+        xBandido += 1;
+    } else {
+        xBandido -= 1;
+    }
+}
+
+int main() {
+    int linhas, colunas;
+    scanf("%d  %d", &linhas, &colunas);
+    scanf("%d  %d", &ymic, &xmic);
+
+    int passeio;
+    scanf("%d", &passeio);
+
+    int direcao;
+    while (passeio > 0) {
+        scanf("%d", &direcao);
+        novaCoordenada(direcao);
+
+        // printf("\n\n%d  %d\n\n", xBandido, yBandido);
+
+        invadiu();
+
+        passeio--;
+    }
+
+    printf("%d\n", minutos);
+}
